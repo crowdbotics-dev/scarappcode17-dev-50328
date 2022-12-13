550c7cb09685079d9d783fb4a51dd011ca969e5f
scartag <tag@crowdbotics.com>
2022-12-13 13:43:51 +0000

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "scarappcode17-dev-50328",
   url: "https://scarappcode17-dev-50328.botics.co",
   api: "https://scarappcode17-dev-50328.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
